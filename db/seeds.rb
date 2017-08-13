27bfb08780c3a8cd2803c1dff1143ef44b7c3a88
José Escorche <josesco5@gmail.com>
2017-08-13 19:40:50 -0300

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create([
+  {name: 'A Game of Thrones', description: 'First book of \'A Song of Ice and Fire\' series' },
+  {name: 'A Clash of Kings', description: 'Second book of \'A Song of Ice and Fire\' series' },
+  {name: 'A Storm of Swords', description: 'Third book of \'A Song of Ice and Fire\' series' },
+  {name: 'A Feast for Crows', description: 'Fourth book of \'A Song of Ice and Fire\' series' },
+  {name: 'A Dance with Dragons', description: 'Fifth book of \'A Song of Ice and Fire\' series' },
+  {name: 'Hunger Games', description: 'Written by Suzanne Collins' },
+  {name: 'The Maze Runner', description: 'Written by James Dashner' }
+])
