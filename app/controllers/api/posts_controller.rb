752b018ba5ea49c2a375197673f88e753483ad23
José Escorche <josesco5@gmail.com>
2017-08-13 19:47:29 -0300

    Filter posts by name and description

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,6 +2,9 @@ module API
   class PostsController < ApplicationController
     def index
       posts = Post.all
+      if filter = params[:filter]
+        posts = posts.where('name ILIKE ? or description ILIKE ?', "%#{filter}%", "%#{filter}%")
+      end
       render json: posts, status: 200
     end
   end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -7,4 +7,17 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime[:json], response.content_type
   end
+
+  test 'get list of posts filtered by name' do
+    got = posts(:got)
+    hunger = posts(:hunger_games)
+
+    get '/api/posts?filter=game'
+    assert_equal 200, response.status
+
+    received_posts = json(response.body)
+    names = received_posts.collect { |p| p[:name] }
+    assert_includes names, got.name
+    assert_includes names, hunger.name
+  end
 end
